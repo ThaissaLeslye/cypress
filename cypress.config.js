aea3c2852107ec4182e2f3474282fc7ed0435dbc
thaissa <thaissa.leslye.tl@gmail.com>
2025-10-11 19:55:41 -0300

    .

--- cypress.config.js
+++ cypress.config.js
@@ -4,22 +4,24 @@ module.exports = defineConfig({
   e2e: {
     experimentalRunAllSpecs: true,
     baseUrl: 'https://playground.bondaracademy.com/',
+    specPattern: 'test/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    charts: true,
-    reportPageTitle: 'Relatório de Testes Cypress',
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    saveAllAttempts: false,
-    // Garante que um ficheiro JSON é gerado
-    json: true,
-  },
-    specPattern: 'test/**/*.cy.{js,jsx,ts,tsx}',
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: false,
+      json: true,
+      charts: true,
+      reportPageTitle: 'Relatório de Testes Cypress',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
+    },
   },
   viewportWidth: 1280,
-  viewportHeight: 720
+  viewportHeight: 720,
 });
-
