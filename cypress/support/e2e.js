4856b0b34505857461e9834a11e7833760c60a45
thaissa <thaissa.leslye.tl@gmail.com>
2025-11-20 13:06:12 -0300

    ;

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,3 +16,27 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import 'cypress-mochawesome-reporter/register';
+
+Cypress.on('fail', (error, runnable) => {
+  // Verifica se o erro veio de um cy.request()
+  if (error.message.includes('cy.request() failed on')) {
+    
+    // Tenta extrair as informações vitais usando Regex
+    const matchUrl = error.message.match(/URL: (.*?)\n/);
+    const matchStatus = error.message.match(/Status: (\d+)/);
+    const matchMethod = error.message.match(/Method: (.*?)\n/);
+
+    const url = matchUrl ? matchUrl[1] : 'URL desconhecida';
+    const status = matchStatus ? matchStatus[1] : 'Erro';
+    const method = matchMethod ? matchMethod[1] : 'REQ';
+
+    // Reescreve a mensagem de erro para ficar limpa no Jenkins
+    error.message = `FALHA API: [${method}] ${url} respondeu com Status ${status}`;
+    
+    // (Opcional) Limpa o stacktrace gigante para não poluir o log
+    // error.stack = error.message; 
+  }
+
+  // Lança o erro modificado para falhar o teste
+  throw error;
+});
